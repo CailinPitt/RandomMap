f76a9c3f97e8e7f977502ecccfbc5d3ccc2479d8
CailinPitt <cailinpitt1@gmail.com>
2016-03-19 22:14:37 -0400

    Started work on restricting lat & long chosen

--- random_map.rb
+++ random_map.rb
@@ -7,16 +7,50 @@ require 'static_map'
 # remove this to get less output when running your bot
 verbose
   
-lat = rand(-90.000000000...90.000000000)
-long = rand(-180.000000000...180.000000000)
-# Get random latitude and longitude
+continent = rand(0..6)
+# Pick which continent to get aerial view of
 
+lat = 0
+long = 0
+# Initialize latitude and longitude
+
+if continent == 0
+	lat = rand(7.208889...72.0)
+	long = rand(-124.733056...-55.620833)
+	# Get random latitude and longitude in North America
+elsif continent == 1
+	lat = rand(-53.896389...12.458611)
+	long = rand(-81.328611...-34.793056)
+	# Get random latitude and longitude in South America
+elsif continent == 2
+	lat = rand(-46.981944...37.346983)
+	long = rand(17.4467...51.25)
+	# Get random latitude and longitude in Africa
+elsif continent == 3
+	lat = rand(36.0042...71.133889)
+	long = rand(-9.500556...66.618056)
+	# Get random latitude and longitude in Europe
+elsif continent == 4
+	lat = rand(1.2661...77.7333)
+	long = rand(-26.0640...169.763162)
+	# Get random latitude and longitude in Asia
+	# Still isn't restricted to Asia 100% of time
+elsif continent == 5
+	lat = rand(-43.643611...10.6833)
+	long = rand(113.155...153.6389)
+	# Get random latitude and longitude in Australia
+else
+	lat = rand(-90.000000000...90.000000000)
+	long = rand(-180.000000000...180.000000000)
+	# Get random latitude and longitude of somewhere in the world (including Antartica)
+end
+puts "#{lat} #{long}"
 image = StaticMap::Image.new({
 size: '640x640',
 sensor: false,
 center: "#{lat.to_s} #{long.to_s}",
 maptype: 'satellite',
-zoom: 10,
+zoom: 12,
 title: 'A map',
 alt: 'Alt text for img html',
 path: './map.png'
