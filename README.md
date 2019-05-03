19a4564439008e3060f85ef7b06d2348d09b5179
Cailin Pitt <cailin.pitt@ge.com>
2019-05-03 09:50:15 -0400

    tweets multiple images, added emojis, fixed concurrency issue, updated readme

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 [@random_map](https://twitter.com/random_map) is a Twitter Bot that runs every several hours on a Raspberry Pi and tweets random aerial images of the Earth. Inspired by the [Earth View extension for Google Chrome](https://chrome.google.com/webstore/detail/earth-view-from-google-ea/bhloflhklmhfpedakmangadcdofhnnoh?hl=en). I had a five hour layover and decided to be productive.
 
 # Code
-Uses three gems:
-* Chatterbot - A really cool Ruby based Twitter Bot framework, used to tweet images
-* static_map - An equally as cool gem that uses Google's Static Map API to generate static aerial images
-* twiter-text - Allows emojis in tweets
+Uses three npm packages:
+* twit - Twitter API wrapper for node, used to tweet images
+* googlemaps - A Google Static Map API wrapper for generating static aerial images
+* node-emoji - Used to add random emojis to tweets :sunglasses:
+* request - Used to download images
+* fs - Used to open image files
+* randomFloat - Generates random numbers used to pick a continent to grab an image from
 
 # Future Work
-* ~~Oceans cover 71% of the Earth's surface. Unfortunately, pictures of water don't look cool. I need to restrict @random_map from calculating latitude & longitude of oceans.~~ Latitude & longitude is now restricted to land
-* In order to trigger the event to send a tweet, Chatterbot searches latest tweets on Twitter for the 'a' character. If it finds a tweet containing the letter 'a' (why wouldn't it?), it tweets a new aerial picture. Pretty dumb, right? Must change this.
-* ~~Emojis are cool~~ Tweets now come with emojis :bowtie: :neckbeard: :sparkles:
+* ~~Oceans cover 71% of the Earth's surface. Unfortunately, pictures of water don't look cool. I need to restrict @random_map from calculating latitude & longitude of oceans.~~ Latitude & longitude is now mostly restricted to land
+* More interesting images?
---
+++ app.js
@@ -0,0 +1,119 @@
+const GoogleMapsAPI = require('googlemaps')
+const fs = require('fs')
+const request = require('request')
+const randomFloat = require('random-float')
+var Twit = require('twit')
+var emoji = require('node-emoji')
+
+var T = new Twit({
+  consumer_key: 'yFlaPrgrNmnu0xGOBUho2fGbR',
+  consumer_secret: 'LcRKpbneTGS6CZbQcAvdGqeXTETNskm0HBCMrN4C6GH7vnIc3y',
+  access_token: '710948858423959552-P6tUyCxoLXGix4YN1tjL4jsWEZ20rqB',
+  access_token_secret: 'G6fcN8lR7KmlEKrrJvpKPFC8ExBHccNEJzPtbC8oiWSLp'
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomIntInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var publicConfig = {
+    key: 'AIzaSyAKfombgAtABCymfaO8G_ESr8OD4Txp1f4'
+};
+
+var continent = getRandomInt(5);
+var zoom = getRandomIntInRange(13, 16);
+
+var message = "Somewhere near"
+var center;
+
+switch(continent) {
+    case 0:
+      center = "" + randomFloat(30.208889, 50.0) + "," + randomFloat(-118.733056, -76.620833)
+      message = message + " North America"
+      break;
+    case 1:
+      center = "" + randomFloat(-13.896389, 1.458611) + "," + randomFloat(-75.328611, -39.793056)
+      message = message + " South America"
+      break;
+    case 2:
+      center = "" + randomFloat(-19.981944, 21.346983) + "," + randomFloat(14.4467, 34.25)
+      message = message + " Africa"
+      break;
+    case 3:
+      center = "" + randomFloat(43.0042, 48.0) + "," + randomFloat(2.500556, 44.618056)
+      message = message + " Europe"
+      break;
+    case 4:
+      center = "" + randomFloat(21.2661, 49.7333) + "," + randomFloat(70.0, 106.0)
+      message = message + " Asia"
+      break;
+    default:
+      center = "" + randomFloat(-31.643611, -20.6833) + "," + randomFloat(116.155, 145.277736)
+      message = message + " Australia"
+}
+
+message += '\n\n' + center + '\n\n' + emoji.random().emoji + ' ' + emoji.random().emoji + ' ' + emoji.random().emoji + ' ' + emoji.random().emoji + ' ' + emoji.random().emoji;
+
+var gmAPI = new GoogleMapsAPI(publicConfig);
+var paramsSatellite = {
+    center: center,
+    zoom: zoom,
+    size: '2000x2000',
+    maptype: 'satellite'
+};
+
+var paramsRoad = {
+    center: center,
+    zoom: zoom,
+    size: '2000x2000',
+    maptype: 'roadmap'
+};
+
+var imageSatellite = gmAPI.staticMap(paramsSatellite);
+
+var streamSatellite = request(imageSatellite).pipe(fs.createWriteStream('satellite.png'));
+
+streamSatellite.on('finish', function() {
+  var imageRoad = gmAPI.staticMap(paramsRoad);
+
+  var streamRoad = request(imageRoad).pipe(fs.createWriteStream('road.png'));
+  
+  streamRoad.on('finish', function() {
+    var satellite = fs.readFileSync('satellite.png', { encoding: 'base64' });
+    var road = fs.readFileSync('road.png', { encoding: 'base64' });
+    
+    T.post('media/upload', { media_data: satellite }, function (err, data, response) {
+      // now we can assign alt text to the media, for use by screen readers and
+      // other text-based presentations and interpreters
+      var mediaIdSatellite = data.media_id_string
+      var satelliteAltText = "Satellite"
+      var meta_params = { media_id: mediaIdSatellite, alt_text: { text: satelliteAltText } }
+    
+      T.post('media/metadata/create', meta_params, function (err, data, response) {
+        if (!err) {
+          T.post('media/upload', { media_data: road }, function (err, data, response) {
+            // now we can assign alt text to the media, for use by screen readers and
+            // other text-based presentations and interpreters
+            var mediaIdRoad = data.media_id_string
+            var roadAltText = "Road"
+            meta_params = { media_id: mediaIdRoad, alt_text: { text: roadAltText } }
+          
+            T.post('media/metadata/create', meta_params, function (err, data, response) {
+              if (!err) {
+                // now we can reference the media and post a tweet (media will attach to the tweet)
+                var params = { status: message, media_ids: [mediaIdSatellite, mediaIdRoad] }
+          
+                T.post('statuses/update', params, function (err, data, response) {
+                })
+              }
+            })
+          })
+        }
+      })
+    })
+  });
+});
