e2d213bc9a57322ccbf47d6edc9a744e9a235912
Cailin Pitt <cailinpitt1@gmail.com>
2020-05-01 19:51:33 -0400

    Cleanup, use ES6 syntax

--- README.md
+++ README.md
@@ -9,14 +9,28 @@
 [@random_map](https://twitter.com/random_map) is a Twitter Bot that runs every several hours on a Raspberry Pi and tweets random aerial images of the Earth. Inspired by the [Earth View extension for Google Chrome](https://chrome.google.com/webstore/detail/earth-view-from-google-ea/bhloflhklmhfpedakmangadcdofhnnoh?hl=en). I had a five hour layover and decided to be productive.
 
 # Code
-Uses three npm packages:
-* twit - Twitter API wrapper for node, used to tweet images
-* googlemaps - A Google Static Map API wrapper for generating static aerial images
-* node-emoji - Used to add random emojis to tweets :sunglasses: :surfer:
-* request - Used to download images
-* fs - Used to open image files
-* randomFloat - Generates random numbers used to pick a continent to grab an image from
+RandomMap picks a random continent (except Antarctica) and generates a random coordinates inside the chosen continent. It then uses the Google Maps API to download satellite and terrain images of the generated coordinates, and tweets the pictures!
 
-# Future Work
-* ~~Oceans cover 71% of the Earth's surface. Unfortunately, pictures of water don't look cool. I need to restrict @random_map from calculating latitude & longitude of oceans.~~ Latitude & longitude is now mostly restricted to land
-* More interesting images?
+# Run
+Run `npm ci` to download project dependencies
+
+Create a `keys.js` file and create objects to hold your Twitter and Google Developer API keys. Make sure to export them, too:
+
+```js
+const google = {
+    key: '...',
+};
+const twitter = {
+    consumer_key: '...',
+    consumer_secret: '...',
+    access_token: '...',
+    access_token_secret: '...'
+};
+
+module.exports = {
+    google,
+    twitter
+};
+```
+
+Now you're all set. Simply do `npm run local`
--- app.js
+++ app.js
@@ -1,150 +1,121 @@
-const GoogleMapsAPI = require('googlemaps')
-const fs = require('fs')
-const request = require('request')
-const randomFloat = require('random-float')
-var Twit = require('twit')
-var emoji = require('node-emoji')
+const axios = require('axios');
+const fs = require('fs-extra');
+const GoogleMapsAPI = require('googlemaps');
+const emoji = require('node-emoji');
+const path = require('path');
+const randomFloat = require('random-float');
 
-var T = new Twit({
-  consumer_key: 'yFlaPrgrNmnu0xGOBUho2fGbR',
-  consumer_secret: 'LcRKpbneTGS6CZbQcAvdGqeXTETNskm0HBCMrN4C6GH7vnIc3y',
-  access_token: '710948858423959552-P6tUyCxoLXGix4YN1tjL4jsWEZ20rqB',
-  access_token_secret: 'G6fcN8lR7KmlEKrrJvpKPFC8ExBHccNEJzPtbC8oiWSLp'
-});
+const { google } = require('./keys.js');
+const {
+  cleanup,
+  getRandomInt,
+  getRandomIntInRange,
+  makePost,
+} = require('./util.js');
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+const gmAPI = new GoogleMapsAPI(google);
 
-function getRandomIntInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const assetDirectory = './assets/';
 
-var publicConfig = {
-    key: 'AIzaSyAKfombgAtABCymfaO8G_ESr8OD4Txp1f4'
-};
+const chooseContinent = () => {
+  let center;
+  const continent = getRandomInt(5);
+  let status = "Somewhere in";
 
-var continent = getRandomInt(5);
-var zoom = getRandomIntInRange(11, 15);
-
-var message = "Somewhere in"
-var center;
-
-switch(continent) {
+  switch (continent) {
     case 0:
-      center = "" + randomFloat(30.208889, 50.0) + ", " + randomFloat(-118.733056, -76.620833)
-      message = message + " North America"
+      center = "" + randomFloat(30.208889, 50.0) + ", " + randomFloat(-118.733056, -76.620833);
+      status = status + " North America";
       break;
     case 1:
-      center = "" + randomFloat(-13.896389, 1.458611) + ", " + randomFloat(-75.328611, -39.793056)
-      message = message + " South America"
+      center = "" + randomFloat(-13.896389, 1.458611) + ", " + randomFloat(-75.328611, -39.793056);
+      status = status + " South America";
       break;
     case 2:
-      center = "" + randomFloat(-19.981944, 21.346983) + ", " + randomFloat(14.4467, 34.25)
-      message = message + " Africa"
+      center = "" + randomFloat(-19.981944, 21.346983) + ", " + randomFloat(14.4467, 34.25);
+      status = status + " Africa";
       break;
     case 3:
-      center = "" + randomFloat(43.0042, 48.0) + ", " + randomFloat(2.500556, 44.618056)
-      message = message + " Europe"
+      center = "" + randomFloat(43.0042, 48.0) + ", " + randomFloat(2.500556, 44.618056);
+      status = status + " Europe";
       break;
     case 4:
-      center = "" + randomFloat(21.2661, 49.7333) + ", " + randomFloat(70.0, 106.0)
-      message = message + " Asia"
+      center = "" + randomFloat(21.2661, 49.7333) + ", " + randomFloat(70.0, 106.0);
+      status = status + " Asia";
       break;
     default:
-      center = "" + randomFloat(-31.643611, -20.6833) + ", " + randomFloat(116.155, 145.277736)
-      message = message + " Australia"
+      center = "" + randomFloat(-31.643611, -20.6833) + ", " + randomFloat(116.155, 145.277736);
+      status = status + " Australia";
+  }
+
+  status += '\n\n' + center + '\n\n' + 
+    emoji.random().emoji + ' ' +
+    emoji.random().emoji + ' ' +
+    emoji.random().emoji + ' ' +
+    emoji.random().emoji + ' ' +
+    emoji.random().emoji;
+
+    return {
+      center,
+      status
+    };
 }
 
-message += '\n\n' + center + '\n\n' + emoji.random().emoji + ' ' + emoji.random().emoji + ' ' + emoji.random().emoji + ' ' + emoji.random().emoji + ' ' + emoji.random().emoji;
-
-var gmAPI = new GoogleMapsAPI(publicConfig);
-var paramsSatellite = {
-    center: center,
-    zoom: zoom,
+const downloadMap = async (center, maptype, imagePath) => {
+  const imageParams = {
+    center,
+    zoom: getRandomIntInRange(11, 15),
+    maptype,
     size: '2000x2000',
-    maptype: 'satellite',
-    scale: 2
-};
+    scale: 2,
+  };
 
-var paramsRoad = {
-    center: center,
-    zoom: zoom,
-    size: '2000x2000',
-    maptype: 'roadmap',
-    scale: 2
-};
+  const imageURL = gmAPI.staticMap(imageParams);
 
-var paramsTerrain = {
-  center: center,
-  zoom: zoom,
-  size: '2000x2000',
-  maptype: 'terrain',
-  scale: 2
-};
+  const image = path.resolve(__dirname, imagePath);
 
-var imageSatellite = gmAPI.staticMap(paramsSatellite);
+  const writer = fs.createWriteStream(image);
 
-var streamSatellite = request(imageSatellite).pipe(fs.createWriteStream('satellite.png'));
-
-streamSatellite.on('finish', function() {
-  var imageRoad = gmAPI.staticMap(paramsRoad);
-
-  var streamRoad = request(imageRoad).pipe(fs.createWriteStream('road.png'));
-  
-  streamRoad.on('finish', function() {
-
-    var imageTerrain = gmAPI.staticMap(paramsTerrain);
-
-    var streamTerrain = request(imageTerrain).pipe(fs.createWriteStream('terrain.png'));
-    
-    streamTerrain.on('finish', function() {
-      var satellite = fs.readFileSync('satellite.png', { encoding: 'base64' });
-      var road = fs.readFileSync('road.png', { encoding: 'base64' });
-      var terrain = fs.readFileSync('terrain.png', { encoding: 'base64' });
-    
-      T.post('media/upload', { media_data: satellite }, function (err, data, response) {
-        // now we can assign alt text to the media, for use by screen readers and
-        // other text-based presentations and interpreters
-        var mediaIdSatellite = data.media_id_string
-        var satelliteAltText = "Satellite"
-        var meta_params = { media_id: mediaIdSatellite, alt_text: { text: satelliteAltText } }
-      
-        T.post('media/metadata/create', meta_params, function (err, data, response) {
-          if (!err) {
-            T.post('media/upload', { media_data: road }, function (err, data, response) {
-              // now we can assign alt text to the media, for use by screen readers and
-              // other text-based presentations and interpreters
-              var mediaIdRoad = data.media_id_string
-              var roadAltText = "Road"
-              meta_params = { media_id: mediaIdRoad, alt_text: { text: roadAltText } }
-            
-              T.post('media/metadata/create', meta_params, function (err, data, response) {
-                if (!err) {
-
-                  T.post('media/upload', { media_data: terrain }, function (err, data, response) {
-                    // now we can assign alt text to the media, for use by screen readers and
-                    // other text-based presentations and interpreters
-                    var mediaIdTerrain = data.media_id_string
-                    var terrainAltText = "Terrain"
-                    meta_params = { media_id: mediaIdTerrain, alt_text: { text: terrainAltText } }
-                  
-                    T.post('media/metadata/create', meta_params, function (err, data, response) {
-                      if (!err) {
-                        // now we can reference the media and post a tweet (media will attach to the tweet)
-                        var params = { status: message, media_ids: [mediaIdSatellite, mediaIdRoad, mediaIdTerrain] }
-                  
-                        T.post('statuses/update', params, function (err, data, response) {
-                        })
-                      }
-                    })
-                  })
-                }
-              })
-            })
-          }
-        })
-      })
-    })
+  const response = await axios({
+    url: imageURL,
+    method: 'GET',
+    responseType: 'stream'
   });
+
+  return new Promise(resolve => response.data.pipe(writer).on('finish', resolve));
+}
+
+const upload = (media_data) => makePost('media/upload', {
+  media_data
+}).then(data => data.media_id_string);
+
+const makeStatusUpdate = (media_ids, status) => makePost('statuses/update', {
+  status,
+  media_ids
 });
+
+const tweet = async (status) => {
+  const media_ids = [];
+  const satellite = fs.readFileSync(assetDirectory + 'satellite.png', { encoding: 'base64' });
+  const terrain = fs.readFileSync(assetDirectory + 'terrain.png', { encoding: 'base64' });
+
+  media_ids.push(await upload(satellite));
+  media_ids.push(await upload(terrain));
+
+  await makeStatusUpdate(media_ids.toString(), status);
+}
+
+const run = async () => {
+  fs.ensureDirSync(assetDirectory);
+
+  const imageInfo = chooseContinent();
+
+  await downloadMap(imageInfo.center, 'satellite', assetDirectory + 'satellite.png');
+  await downloadMap(imageInfo.center, 'terrain', assetDirectory + 'terrain.png');
+
+  tweet(imageInfo.status);
+
+  cleanup(assetDirectory);
+}
+
+run();
